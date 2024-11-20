adbbf60ac1792a2e18ae67dfc51375aa6fc92380
hyuuhoang <Huyhoangzz2k5@gmail.com>
2024-11-20 21:55:52 +0700

    Add files via upload

---
+++ session 2 IT2 bài 5.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+	int chieudai = 15;
+	int chieurong = 20;
+	
+	int chuvi = (chieudai + chieurong) * 2;
+	printf("chu vi hinh chu nhat la %d \n",chuvi);
+	
+	int dientich = chieudai * chieurong;
+	printf("dien tich hinh chu nhat la %d \n",dientich); 
+
+	return 0; 
+} 
