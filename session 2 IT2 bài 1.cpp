a7d78a6dfcf73756a2fdc634c808d9fa48419c19
hyuuhoang <Huyhoangzz2k5@gmail.com>
2024-11-20 19:50:12 +0700

    Add files via upload

---
+++ session 2 IT2 bài 1.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h> 
+
+int main(){
+	// khai bao bien kieu du lieu int
+	int age;
+	
+	// khai bao bien kieu du lieu int va gan gia tri 
+	int  year = 2024;
+	
+	// truy xuat de lay gia tri cua bien 
+	printf("nam nay la %d",year);
+	 
+	 // thay doi ga tri cua bien age
+	 age = 18;
+	 printf("\n nam nay toi %d tuoi",age); 
+	 
+	 // khai bao bien kieu du lieu char 
+	 char kyTu = 'a';
+	 printf("Gia tri cua bien kyTu la %c",kyTu);
+	 
+	 // khai bao bien kieu du lieu float
+	 float pi = 3.14;
+	 printf("\n gia tri cua pi la %.2f",pi);
+	 
+	return 0;
+}
