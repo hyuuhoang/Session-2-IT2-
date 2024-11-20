bff438b258ce49f209e98386a314b27956922ad6
hyuuhoang <Huyhoangzz2k5@gmail.com>
2024-11-20 21:30:39 +0700

    Add files via upload

---
+++ session 2 IT2 bài 4.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int canh = 15;
+	
+	
+	int chuViHinhVuong = 4 * canh;
+	printf("chu vi hinh vuong la %d \n",chuViHinhVuong);
+	
+	int  dienTichHinhVuong = canh * canh;
+	printf("dien tich hinh vuong la %d \n",dienTichHinhVuong);
+	
+	return 0;  
+} 
