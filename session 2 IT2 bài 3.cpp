a8d875e7342110f1fbf5a5193bbd12204d72d4ba
hyuuhoang <Huyhoangzz2k5@gmail.com>
2024-11-20 21:07:09 +0700

    Add files via upload

---
+++ session 2 IT2 bài 3.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+	int so1 = 10;
+	int so2 = 20;
+	
+	int tong = so1 + so2;
+	printf("tong hai so la %d \n",tong);
+	
+	int hieu = so1 - so2;
+	printf("hieu hai so la %d \n",hieu);
+	
+	int tich = so1 * so2;
+	printf("tich hai so la %d \n",tich);
+	
+	int thuong = so1 / so2;
+	printf("thuong hai so la %d \n",thuong); 
+	
+	return 0; 
+} 
